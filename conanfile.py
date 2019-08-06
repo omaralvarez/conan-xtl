cdcc6532fdb22c3f721757a22e18d4ae6c1d98ca
Omar Alvarez <omar.alvarez@cinfo.es>
2019-08-06 10:26:17 +0200

    Initial version

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, CMake, tools
+
+
+class XtlConan(ConanFile):
+    name = "xtl"
+    version = "0.6.5"
+    license = "BSD 3-Clause"
+    #author = "<Put your name here> <And your email here>"
+    url = "https://github.com/omaralvarez/conan-xtl"
+    repo_url = "https://github.com/QuantStack/xtl"
+    description = "The x template library"
+    topics = ("templates")
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    exports_sources = "*.hpp"
+    no_copy_source = True
+
+    def source(self):
+        self.run("git clone -b '%s' --single-branch --depth 1 %s" % (self.version, self.repo_url))
+    
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="xtl")
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+    
+    def package_id(self):
+        self.info.header_only()
