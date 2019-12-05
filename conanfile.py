c7710fb72f79148b4f90e7e908490600c16b3c4b
Omar Alvarez <omar.alvarez@cinfo.es>
2019-12-05 16:46:22 +0100

    Update to 0.6.8

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class XtlConan(ConanFile):
     name = "xtl"
-    version = "0.6.7"
+    version = "0.6.8"
     license = "BSD 3-Clause"
     #author = "<Put your name here> <And your email here>"
     url = "https://github.com/omaralvarez/conan-xtl"
@@ -16,7 +16,7 @@ class XtlConan(ConanFile):
     no_copy_source = True
 
     def source(self):
-        self.run("git clone -b '%s' --single-branch --depth 1 %s" % (self.version, self.repo_url))
+        self.run("git clone -b %s --single-branch --depth 1 %s" % (self.version, self.repo_url))
     
     def _configure_cmake(self):
         cmake = CMake(self)
